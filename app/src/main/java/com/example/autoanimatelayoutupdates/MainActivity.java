8456e552e4e1d5065c64f87d12f6672664b04d74
Kanha Tomar <kanharajput91@gmail.com>
2022-03-18 13:05:15 +0530

    Scene created and referenced

--- app/src/main/java/com/example/autoanimatelayoutupdates/MainActivity.java
+++ app/src/main/java/com/example/autoanimatelayoutupdates/MainActivity.java
@@ -4,17 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.transition.Scene;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    // creating objects for scenes
+    Scene aScene;
+    Scene anotherScene;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // creating the scene root from the layout file
+        ViewGroup sceneRoot = findViewById(R.id.scene_root);
+
+        // create the scenes
+        aScene = Scene.getSceneForLayout(sceneRoot, R.layout.a_scene,this);
+        anotherScene = Scene.getSceneForLayout(sceneRoot, R.layout.another_scene,this);
     }
 }
