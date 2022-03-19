e185c901cc66d6c6b2bf760eb704fff237204798
Kanha Tomar <kanharajput91@gmail.com>
2022-03-19 14:47:44 +0530

    Custom Transition between Scenes added

--- app/src/main/java/com/example/autoanimatelayoutupdates/CustomTransition.java
+++ app/src/main/java/com/example/autoanimatelayoutupdates/CustomTransition.java
@@ -1,23 +1,64 @@
 package com.example.autoanimatelayoutupdates;
 
 import android.animation.Animator;
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.transition.Transition;
 import android.transition.TransitionValues;
+import android.view.View;
 import android.view.ViewGroup;
 
 public class CustomTransition extends Transition {
+
+    // create a variable to store property in map
+    private static final String PROPNAME_BACKGROUND = "customTransition:change_color:background";
+
     @Override
     public void captureStartValues(TransitionValues transitionValues) {
-
-    }
-
-    @Override
-    public Animator createAnimator(ViewGroup sceneRoot, TransitionValues startValues, TransitionValues endValues) {
-        return super.createAnimator(sceneRoot, startValues, endValues);
+        captureValues(transitionValues);                // get the value from the first scene
     }
 
     @Override
     public void captureEndValues(TransitionValues transitionValues) {
+        captureValues(transitionValues);               // get the desired values from the second scene
+    }
 
+    // method to capture value and store in the transitionValues map
+    private void captureValues(TransitionValues transitionValues) {
+        transitionValues.values.put(PROPNAME_BACKGROUND,transitionValues.view.getBackground());
+    }
+
+    @Override
+    public Animator createAnimator(ViewGroup sceneRoot, TransitionValues startValues, TransitionValues endValues) {
+
+        // check startvalues and endvalues are not null
+        if(startValues == null || endValues == null) {
+            return null;
+        }
+
+        final View view = endValues.view;       // as both startValues and endValues have the same view
+
+        // getting the background of the views from the startscene values and endscene values, which are saved by us
+        Drawable startBackground = (Drawable) startValues.values.get(PROPNAME_BACKGROUND);
+        Drawable endBackground = (Drawable) endValues.values.get(PROPNAME_BACKGROUND);
+
+        // get the color from the Drawable class
+        if(startBackground instanceof ColorDrawable && endBackground instanceof ColorDrawable) {
+            ColorDrawable startColor = (ColorDrawable) startBackground;
+            ColorDrawable endColor = (ColorDrawable) endBackground;
+
+            if(startColor.getColor() != endColor.getColor()) {
+                ObjectAnimator colorAnim = ObjectAnimator.ofInt(view, "textColor", startColor.getColor(), endColor.getColor());
+                colorAnim.setEvaluator(new ArgbEvaluator());
+                colorAnim.setDuration(2000);
+                colorAnim.start();
+
+                return colorAnim;
+            }
+        }
+        return null;
     }
 }
--- app/src/main/java/com/example/autoanimatelayoutupdates/CustomTransitionActivity.java
+++ app/src/main/java/com/example/autoanimatelayoutupdates/CustomTransitionActivity.java
@@ -2,13 +2,49 @@ package com.example.autoanimatelayoutupdates;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ArgbEvaluator;
+import android.animation.ObjectAnimator;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.transition.Scene;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 public class CustomTransitionActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private Scene firstScene;
+    private Scene secondScene;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_transition);
+
+        // change the title color
+        textView = findViewById(R.id.title_textview);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ObjectAnimator colorAnim = ObjectAnimator.ofInt(v, "textColor", Color.RED, Color.GREEN);
+                colorAnim.setEvaluator(new ArgbEvaluator());
+                colorAnim.setDuration(2000);
+                colorAnim.start();
+            }
+        });
+
+        FrameLayout container = findViewById(R.id.frame_layout);
+
+        // inflate the scenes
+        firstScene =  Scene.getSceneForLayout(container,R.layout.first_scene,this);
+        secondScene = Scene.getSceneForLayout(container,R.layout.second_scene,this);
+
+    }
+
+    public void goToSecondScene(View view) {
+        CustomTransition customTransition = new CustomTransition();
+        TransitionManager.go(secondScene,customTransition);
     }
 }
